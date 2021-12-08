d8d50ba8ef49a763b9ca68cc394983186a173979
Carter Dooley <carter.j.dooley43@gmail.com>
2021-12-08 14:57:22 -0600

    Init

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Boba";
+        String lastName = "Fett";
+//    Year in BBY
+        int birthYear = 32;
+        String birthWorld = "Kamino";
+//    Height in Meters
+        float charHeight = 1.83f;
+//    Weight in kilograms
+        int charWeight = 78;
+        float purse = 150.0f;
+
+//        Shopping spree
+        float driod = 24.3f;
+        float boots = 45f;
+
+        purse -= driod;
+        purse -= boots;
+        purse *= 2;
+        float aTenth = (purse / 10);
+        purse -= aTenth;
+        System.out.println(purse);
+//    LightSaber Part 3
+        
+    }
+}
+
+
